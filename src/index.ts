464a7280dbaee75fc7c59a27497fd6bc1fe3ab70
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2020-06-07 05:37:59 -0600

    a bit more detailed model

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import * as blackjack from './blackjack';
+import * as blackjack from './blackjack'
+import * as tf from '@tensorflow/tfjs'
+import { computer } from './tfjs'
 
 const hitButton: HTMLButtonElement = document.querySelector("#hit-button")
 const standButton: HTMLButtonElement = document.querySelector("#stand-button")
--- src/tfjs.ts
+++ src/tfjs.ts
@@ -2,17 +2,43 @@ import * as tf from '@tensorflow/tfjs';
 
 // Define a model for linear regression.
 const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+model.add(tf.layers.dense({
+	units: 21,
+	activation: 'sigmoid',
+	inputShape: [10]
+}))
+model.add(tf.layers.dense({
+	units: 1,
+	activation: 'sigmoid'
+}))
 
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+model.compile({
+	loss: tf.losses.meanSquaredError,
+	optimizer: tf.train.sgd(0.1)
+})
 
 // Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+const xs = tf.tensor2d([
+		[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+		[4, 1, 2, 1, 2, 3, 1, 5, 1, 2],
+], [2, 10])
+const ys = tf.tensor2d([
+	[1000],
+	[1],
+], [2, 1])
 
 // Train the model using the data.
-model.fit(xs, ys, {epochs: 10}).then(() => {
-  // Use the model to do inference on a data point the model hasn't seen before:
-  model.predict(tf.tensor2d([5], [1, 1])).print();
-  // Open the browser devtools to see the output
+model.fit(xs, ys, {epochs: 4}).then((resp) => {
+	// Use the model to do inference on a data point the model hasn't seen before:
+	//model.predict(tf.tensor2d([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 10])).print()
+	// Open the browser devtools to see the output
+	console.log('%c Training History:', 'color: #6060e0; font-size: 18px;')
+	console.table(resp.history)
+	model.predict(tf.tensor2d([1,2,3,4,5,6,7,8,9,0],[1, 10])).print()
 });
+
+const computer = (input: Array<any>) => {
+	return model.predict(tf.tensor2d(input, [1, 10]))
+}
+
+export { computer }
